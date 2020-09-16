5f019abc781237e015850c9b16f0f28ba9f84f31
Sander Bakkum <sanderbakkum@MacBook-Pro-van-Sander.local>
2020-09-16 15:03:29 +0200

    fasfgsgsgsfa

---
+++ gedicht.py
@@ -0,0 +1,12 @@
+print("Hallo ik ben Sander")
+print("Dit is een klein gedichtje")
+
+vers1 = 'dit is een gedicht en het is wat licht' 
+vers2 = 'ik vind gamen leuk om te doen, je hebt een stink schoen' 
+vers3 = 'ik vind een gedicht niet leuk om te maken, maar ik moet me maar vermaken'
+vers4 = 'dit was het gedicht. Deze is niet voor je nicht'
+
+print(vers1)
+print(vers2)
+print(vers3)
+print(vers4)

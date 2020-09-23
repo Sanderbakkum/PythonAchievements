a345f309f6c9bff438e3850e57edc2036f5276a0
Sander Bakkum <sanderbakkum@MacBook-Pro-van-Sander.local>
2020-09-23 14:53:04 +0200

    Python

---
+++ PYTB1L2InterestingTellMeMore.py
@@ -0,0 +1,8 @@
+print("Hey ik ben Sander hoe heet jij?")
+naam = input()
+print("Dus je heet " + naam + " Leuk hoe oud ben je?")
+leetijd = input()
+print("Oke " + naam + " je bent dus " + leetijd + " jaar oud.")
+print(naam + " waar woon je?")
+plaats = input()
+print("Oke " + naam + " je bent dus " + leetijd + " jaar oud en woont in " + plaats)
---
+++ PYTB1L2MyFirstOperations.py
@@ -0,0 +1,21 @@
+som1 = 57457 + 5352
+print("57457 + 5352 =")
+print(som1)
+
+som2 = som1 - 47432
+print("62809 - 47432 =")
+print(som2)
+
+som3 = som2 / 27
+print("15377 / 27 =")
+print(som3)
+
+som4 = 569 * 3
+print("569 * 3 =")
+print(som4)
+
+som5 = 5361 % 0.9
+print("5361 % 0.9")
+print(som5)
+
+2541 += 4
